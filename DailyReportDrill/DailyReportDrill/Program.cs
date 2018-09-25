9e1a1c1843bf8784c059ffaefefc2fb0d3af127a
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-09-24 21:32:26 -0600

    Adding Daily Report drill
    First commit of daily report drill

---
+++ DailyReportDrill/DailyReportDrill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DailyReportDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool helpNeeded = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperience = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string additionalFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            decimal studyHours = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+            
+        }
+    }
+}
