25babeef134e595b3392eef34c3350d8b419f5e1
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-09-26 21:04:39 -0600

    Basic Approval Program
    first commit

---
+++ BasicApprovalProgram/BasicApprovalProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace BasicApprovalProgram
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please type \"yes\" or \"no\"");
+            bool dui = "yes".Equals(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            bool qualified = (age > 15 && dui == false && tickets < 3);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
