bc497a3bcd013e89bb43cd8fc687aa45dd49ce50
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-09-27 18:58:59 -0600

    While and Do While drill
    initial commit

---
+++ WhileAndDoWhile/WhileAndDoWhile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WhileAndDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            Console.WriteLine("This program will use a while loop to count to 10.");
+            Console.WriteLine("Press enter to start.");
+            Console.ReadLine();
+
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int a = 10;
+
+            Console.WriteLine("Now it will count back down using a do while loop.");
+            Console.WriteLine("Press enter to start.");
+            Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine(a);
+                a--;
+            } while (a > 0);
+
+            Console.ReadLine();
+
+        }
+    }
+}
