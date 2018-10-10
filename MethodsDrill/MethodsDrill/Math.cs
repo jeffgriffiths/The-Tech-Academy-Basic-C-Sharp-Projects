87a64cd014ff9f641e158ce4efb1b774a4a8c152
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-09 21:41:48 -0600

    MethodDrill
    Initial commit

---
+++ MethodsDrill/MethodsDrill/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill
+{
+    public class Math
+    {
+        public int AddNumber(int num1)
+        {
+            int resultAdd = num1 + 2;
+            return resultAdd;
+        }
+
+        public int DivideNumber(int num1)
+        {
+            int resultDivide = num1 / 2;
+            return resultDivide;
+        }
+
+        public int MultiplyNumber(int num1)
+        {
+            int resultMultiply = num1 * 2;
+            return resultMultiply;
+        }
+
+    }
+}
---
+++ MethodsDrill/MethodsDrill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number and I'll run some operations on it.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+            Console.WriteLine("Adding 2 to your number...");
+            int sum = math.AddNumber(num1);
+            Console.WriteLine(sum);
+
+            Console.WriteLine("Dividing your number by 2...");
+            int quotient = math.DivideNumber(num1);
+            Console.WriteLine(quotient);
+
+            Console.WriteLine("Multiplying your number by 2...");
+            int product = math.MultiplyNumber(num1);
+            Console.WriteLine(product);
+
+            Console.WriteLine("That's it...hit Enter to end.");
+            Console.ReadLine();
+        }
+    }
+}
