ad92d2020e9c185ece01061b75b9ff34256755dd
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-10 19:29:49 -0600

    MethodDrill3
    Initial Commit

---
+++ MethodDrill3/MethodDrill3/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    class Math
+    {
+        public int DoMathStuff(int inputInt)
+        {
+            int resultAdd = inputInt + 2;
+            return resultAdd;
+        }
+
+        public int DoMathStuff(double inputDecimal)
+        {
+            int resultDivide = Convert.ToInt32(inputDecimal) / 2;
+            return resultDivide;
+        }
+
+        public int DoMathStuff(string inputString)
+        {
+            int resultMultiply = Convert.ToInt32(inputString) * 2;
+            return resultMultiply;
+        }
+    }
+}
---
+++ MethodDrill3/MethodDrill3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math add = new Math();
+            Math divide = new Math();
+            Math multiply = new Math();
+
+            Console.WriteLine("Please enter an integer.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int sum = add.DoMathStuff(inputInt: num1);
+            Console.WriteLine("The result of the first call is " + sum + ".");
+
+            Console.WriteLine("Please enter a decimal number.");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            int quotient = divide.DoMathStuff(inputDecimal: num2);
+            Console.WriteLine("The result of the second call is " + quotient + ".");
+
+            Console.WriteLine("Please enter a last number.");
+            string num3 = Console.ReadLine();
+            int product = multiply.DoMathStuff(inputString: num3);
+            Console.WriteLine("The result of the second call is " + product + ".");
+
+            Console.ReadLine();
+
+        }
+    }
+}
