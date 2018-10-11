f81ab68316bf6cfce8c8118db5673c45e50b7b91
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-10 18:55:03 -0600

    Method Drill Two
    Initial Commit

---
+++ MethodDrill2/MethodDrill2/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill2
+{
+    public class Math
+    {
+        public void doStuff(int numberOne, int numberTwo)
+        {
+            int quotient = numberOne / 2;
+            Console.WriteLine("The second number is: " + numberTwo);
+        }
+    }
+}
---
+++ MethodDrill2/MethodDrill2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Math number = new Math();
+            number.doStuff(1, 2);
+            number.doStuff(numberOne: 3, numberTwo: 4);
+            Console.ReadLine();
+        }
+    }
+}
