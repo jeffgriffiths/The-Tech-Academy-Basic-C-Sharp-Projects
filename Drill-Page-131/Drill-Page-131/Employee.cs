e6db6dc6221095d80d598a236de3fff96d136a17
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-12 17:05:31 -0600

    Drill Page 131
    Initial Commit

---
+++ Drill-Page-131/Drill-Page-131/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_131
+{
+    class Employee<T> : Person, IQuitTable
+    {
+        public int EmployeeId { get; set; }
+        public List<T> Things { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("The student above has thrown their keyboard against the wall and quit.");
+        }
+
+        public void ListThings()
+        {
+            foreach (T thing in Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+
+        public static implicit operator Employee<T>(List<string> v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator Employee<T>(List<int> v)
+        {
+            throw new NotImplementedException();
+        }
+
+        //public string CheckId(Employee employeeOne, Employee employeeTwo)
+        //{
+        //    bool compareId = employeeOne.EmployeeId == employeeTwo.EmployeeId;
+        //    string result = "";
+        //    if (compareId)
+        //    {
+        //        result = "The Employee IDs match.";
+        //    }
+        //    else
+        //    {
+        //        result = "The Employee IDs do not match.";
+        //    }
+        //    return result;
+        //}
+    }
+}
---
+++ Drill-Page-131/Drill-Page-131/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeeOne = new Employee<string>();
+            Employee<int> employeeTwo = new Employee<int>();
+            employeeOne.Things = new List<string>() { "Watch", "Belt", "Hat" };
+            employeeTwo.Things = new List<int>() { 1, 2, 3 };
+
+            Console.WriteLine("Going to write out the employee things lists. Press any key...");
+            Console.ReadKey();
+
+            employeeOne.ListThings();
+            employeeTwo.ListThings();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
