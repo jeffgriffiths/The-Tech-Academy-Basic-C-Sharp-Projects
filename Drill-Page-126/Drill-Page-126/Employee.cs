a11c5915bcb9719cf0fa42e020dccd9cb7da7dce
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-11 18:40:35 -0600

    Drill Page 126
    Initial Commit

---
+++ Drill-Page-126/Drill-Page-126/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_126
+{
+    class Employee : Person, IQuitTable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("The student above has thrown their keyboard against the wall and quit.");
+        }
+    }
+}
---
+++ Drill-Page-126/Drill-Page-126/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            IQuitTable employeeQuit = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+            employeeQuit.Quit();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
