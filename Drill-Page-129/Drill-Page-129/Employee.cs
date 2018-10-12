7bc29df5982c672dc932e01ce708734ada672038
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-11 21:39:23 -0600

    Drill Page 129
    Initial Commit

---
+++ Drill-Page-129/Drill-Page-129/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_129
+{
+    class Employee : Person, IQuitTable
+    {
+        public int EmployeeId { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("The student above has thrown their keyboard against the wall and quit.");
+        }
+
+        public string CheckId(Employee employeeOne, Employee employeeTwo)
+        {
+            bool compareId = employeeOne.EmployeeId == employeeTwo.EmployeeId;
+            string result = "";
+            if (compareId)
+            {
+                result = "The Employee IDs match.";
+            }
+            else
+            {
+                result = "The Employee IDs do not match.";
+            }
+            return result;
+        }
+    }
+}
---
+++ Drill-Page-129/Drill-Page-129/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee();
+            Employee employeeTwo = new Employee();
+
+            Console.WriteLine("Lets check if the employee IDs match...");
+            Console.WriteLine("Input the first employees ID and press Enter.");
+            employeeOne.EmployeeId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the second employees ID and press Enter.");
+            employeeTwo.EmployeeId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(employeeOne.CheckId(employeeOne, employeeTwo));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
