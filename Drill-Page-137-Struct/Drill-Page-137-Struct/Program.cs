07e4a58915c8661abb27db38bdce51aa09922ddd
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-12 18:37:16 -0600

    Drill Page 137 Struct
    Initial Commit

---
+++ Drill-Page-137-Struct/Drill-Page-137-Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_137_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number straightCashHomie = new Number();
+
+            straightCashHomie.Amount = 300.5m;
+
+            Console.WriteLine("This is the amount assigned to the Number variable:");
+            Console.WriteLine(straightCashHomie.Amount);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
