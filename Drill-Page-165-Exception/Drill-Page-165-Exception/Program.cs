53839009a4fb8d1ab2ccb65e2d6840f506d9fa94
jeff.griffiths <jeffrey.griffiths@outlook.com>
2018-10-17 19:23:15 -0600

    Drill Page 164 Exceptions
    Initial Commit

---
+++ Drill-Page-165-Exception/Drill-Page-165-Exception/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill_Page_165_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");            
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+                if (age == 0 || age < 0)
+                {
+                    throw (new UseRealAgeException("Negative and 0 are not an allowable age"));   
+                }
+                else
+                {
+                    var today = DateTime.Today.Year;
+                    var yearOfBirth = today - age;
+                    Console.WriteLine("You were born in:");
+                    Console.WriteLine(yearOfBirth);
+                }
+            }
+            catch (UseRealAgeException oex)
+            {
+                Console.WriteLine(oex.Message.ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error, you entered something other than an integer.");
+            }
+            finally
+            {
+                Console.WriteLine("Press enter to end.");
+                Console.ReadLine();
+            }
+            
+        }
+        
+        public class UseRealAgeException : Exception
+        {
+            public UseRealAgeException(string message)
+                : base(message)
+            {
+            }
+        }
+    }   
+}
